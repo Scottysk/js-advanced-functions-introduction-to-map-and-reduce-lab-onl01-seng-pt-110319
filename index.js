669b7f6dc14b1483df6401f799110b1850c8bd08
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-23 21:41:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+  return sourceArray.map(num => num * -1);
+}
+
+
+
+function mapToNoChange(sourceArray) {
+  return sourceArray;
+}
+
+
+function mapToDouble(sourceArray) {
+  return sourceArray.map(num => num * 2);
+}
+
+
+function mapToSquare(sourceArray) {
+  return sourceArray.map(num => num ** 2);
+}
+
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let total = startingPoint;
+  for(let i=0; i < sourceArray.length; i++) {
+    total += sourceArray[i];
+  }
+  return total;
+}
+
+function reduceToAllTrue(sourceArray) {
+  return sourceArray.reduce(function(total, value) {
+    if (total && value) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+function reduceToAnyTrue(sourceArray) {
+  return sourceArray.reduce(function(total, value) {
+    if (total || value) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
